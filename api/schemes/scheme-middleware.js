09ad81398ed9af758bd2e0a5e814b5e4005b945e
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-12 11:10:51 -0700

    add middleware functions to scheme-middleware.js

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Scheme = require('./scheme-model')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +8,13 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  const valid = await Scheme.findById(req.params.id)
+  if(!valid) {
+    next(res.status(404).json({ "message": "scheme with scheme_id" + req.params.id + " not found" }))
+  } else {
+    next()
+  }
 }
 
 /*
@@ -19,7 +26,12 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { scheme_name } = req.body;
+  if(!scheme_name || scheme_name.trim() === '' || typeof scheme_name !== 'string') {
+    next(res.status(400).json({ "message": "invalid scheme_name" }))
+  } else {
+    next()
+  }
 }
 
 /*
@@ -32,7 +44,12 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
-
+const {instructions, step_number} = req.body;
+if(!instructions || instructions.trim() === '' || typeof instructions !== 'string' || typeof step_number !== 'number' || step_number < 1) {
+  next(res.status(400).json({ "message": "invalid step"}))
+} else  {
+  next()
+}
 }
 
 module.exports = {
