2e775675dae421e9262266df1d0dfb2a99f9a7b3
Kyle Andrus <kyleandrus3@gmail.com>
2023-01-12 13:55:13 -0700

    add findSteps function to scheme-model.js

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -102,12 +102,6 @@ return db.select('sc.scheme_name', st.*)
     .where('sc.scheme_id', `${scheme_id}`)
     .orderBy('st.step_number', 'asc')
 
-  // let result = {
-  //   scheme_id: scheme[0].scheme_id ? scheme[0].scheme_id : 'none',
-  //   scheme_name: scheme[0].scheme_name,
-  //   steps: []
-  // }
-
   let result = scheme.reduce((acc, currentVal) => {
     if(currentVal.instructions) {
     acc.steps.push({step_id: currentVal.step_id, step_number: currentVal.step_number, instructions: currentVal.instructions})
@@ -119,7 +113,7 @@ return db.select('sc.scheme_name', st.*)
 }
 
 
-function findSteps(scheme_id) { // EXERCISE C
+async function findSteps(scheme_id) { // EXERCISE C
   /*
     1C- Build a query in Knex that returns the following data.
     The steps should be sorted by step_number, and the array
@@ -140,6 +134,20 @@ function findSteps(scheme_id) { // EXERCISE C
         }
       ]
   */
+      const scheme = await db.select('sc.scheme_name', 'st.*', 'sc.scheme_id')
+      .from('schemes as sc')
+      .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+      .where('sc.scheme_id', `${scheme_id}`)
+      .orderBy('st.step_number', 'asc')
+
+      let result = scheme.reduce((acc, currentVal) => {
+        if(currentVal.instructions) {
+        acc.push({step_id: currentVal.step_id, step_number: currentVal.step_number, instructions: currentVal.instructions, scheme_name: currentVal.scheme_name})
+        }
+        return acc
+      }, [])
+      return result
+    
 }
 
 function add(scheme) { // EXERCISE D
